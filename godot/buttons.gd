b06063c7f7cf3a481e5624e9480d25dfa0a3e248
Guillaume327 <guillaume.kucia@epitech.eu>
2021-12-04 14:16:15 +0100

    add turrets

--- godot/buttons.gd
+++ godot/buttons.gd
@@ -1,11 +1,12 @@
-extends Node
+extends Button
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 export var button_name = ""
-
+onready var template_turret1 = preload("res://Turret1.tscn")
+onready var turret_container = get_node("/root/node/Turrets")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,6 +18,9 @@ func _button_quit():
 	pass
 	
 func _add_turret1():
+	var turret = template_turret1.instance()
+	turret.position = rect_global_position
+	turret_container.add_child(turret)
 	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):

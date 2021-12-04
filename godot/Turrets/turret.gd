98e0a78a23b5d7dbf39165aa2ab0969f0b008e9c
Guillaume327 <guillaume.kucia@epitech.eu>
2021-12-04 16:04:27 +0100

    vector

--- godot/Turrets/turret.gd
+++ godot/Turrets/turret.gd
@@ -1,18 +1,28 @@
 extends Node2D
 
-var reloading = 0
+export var attack_speed = 0.00
+onready var template_bullet = preload("res://Bullet.tscn")
+
+var speed_incr = 0
 
 func _process(_delta):
-	#track()
+	track()
 	pass
 
-#func track():
-	#var enemy_position = get_parent().get_node("Path/enemy/Kinematic/Sprite").position
-	#get_node("canon").look_at(enemy_position)
-	#if (reloading > 30):
-		#shoot(enemy_position)
+func track():
+	var enemy_position = get_node("/root/node/Path/enemy").position
+	get_node("canon").look_at(enemy_position)
+	speed_incr += 1
+	if (speed_incr > attack_speed * 60):
+		shoot(enemy_position)
+		speed_incr = 0
 
-#func shoot(enemy_position):
-	#get_node("bullet").look_at(enemy_position)
-	#pass
+func shoot(enemy_position):
+	var bullet = template_bullet.instance()
+	print(global_position)
+	var pos = enemy_position - global_position
+	bullet.get_node("bullet").velocity = (pos / 10)
+	bullet.position = Vector2(0,0)
+	add_child(bullet)
+	pass
 

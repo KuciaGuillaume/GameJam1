9fed7a0ae75f7778899304dd524d841c89994bcf
Guillaume327 <guillaume.kucia@epitech.eu>
2021-12-04 14:24:53 +0100

    scene perso

--- godot/Turrets/turret.gd
+++ godot/Turrets/turret.gd
@@ -3,15 +3,16 @@ extends Node2D
 var reloading = 0
 
 func _process(_delta):
-	track()
+	#track()
 	pass
 
-func track():
-	var enemy_position = get_parent().get_node("Path/enemy/Kinematic/Sprite").position
-	get_node("canon").look_at(enemy_position)
-	if (reloading > 30):
-		shoot(enemy_position)
+#func track():
+	#var enemy_position = get_parent().get_node("Path/enemy/Kinematic/Sprite").position
+	#get_node("canon").look_at(enemy_position)
+	#if (reloading > 30):
+		#shoot(enemy_position)
 
-func shoot(enemy_position):
-	get_node("bullet").look_at(enemy_position)
-	pass
+#func shoot(enemy_position):
+	#get_node("bullet").look_at(enemy_position)
+	#pass
+

2d9781711f01ff31c9f1aa0246f8949c6b504f42
Guillaume327 <guillaume.kucia@epitech.eu>
2021-12-04 11:57:10 +0100

    srcipt buttons

---
+++ godot/buttons.gd
@@ -0,0 +1,20 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+export var button_name = ""
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.connect("pressed", self, button_name)
+	pass # Replace with function body.
+
+func _button_quit():
+	OS.window_fullscreen = !OS.window_fullscreen
+	pass
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
